e14dbb547089c6296e501073d9c17a09e087e500
vladzimir.blr13@gmail.com <vladzimir.blr13@gmail.com>
2021-12-01 18:08:44 +0300

    hw.13

---
+++ HW.13.Task2/Motorcycle.cs
@@ -0,0 +1,24 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW._13.Task2
+{
+    class Motorcycle
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Odometre { get; set; }
+
+        public override string ToString()
+        {
+            Log.Information("The proces data to Console");
+            return $"Id:{Id}, Name: {Name}, Model: {Model}, Odometre: {Odometre}, Year: {Year}";
+        }
+    }
+}
---
+++ HW.13.Task2/MotorcycleRepository.cs
@@ -0,0 +1,55 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW._13.Task2
+{
+    class MotorcycleRepository : IMotorcycleRepository
+    {
+        private static List<Motorcycle> _motosicle = new List<Motorcycle>();
+        public MotorcycleRepository(List<Motorcycle> motorcycles)
+        {
+            _motosicle = motorcycles;
+            Log.Information("Grud started");  
+        }
+        protected List<Motorcycle> moto 
+        {
+            get => _motosicle;
+            set => _motosicle = value;
+        }
+        public void CreateMotorcycle(Motorcycle motorcycle)
+        {
+            _motosicle.Add(motorcycle);
+            Log.Debug("Add Motorcycle" + motorcycle.Name);
+        }
+        public void DeleteMotorcycle(int idMotorcycle)
+        {
+            var motorcycle = GetMotorcycleById(idMotorcycle);
+            _motosicle.Remove(motorcycle);
+            Log.Debug($"Motorcyle {motorcycle.Name}, {motorcycle.Id} is deleted");
+        }
+        public Motorcycle GetMotorcycleById(int motrcycleId)
+        {
+            var motorcycle = _motosicle.Find(x => x.Id == motrcycleId);
+            return motorcycle;
+            Log.Debug($"motorcycle with Id: {motrcycleId}");
+        }
+        public void UpdateMotorcycle(Motorcycle motorcycle)
+        {
+            int index = _motosicle.FindIndex(f => f.Id == motorcycle.Id);
+            _motosicle[index].Name = motorcycle.Name;
+            _motosicle[index].Year = motorcycle.Year;
+            _motosicle[index].Model = motorcycle.Model;
+            _motosicle[index].Odometre = motorcycle.Odometre;
+            Log.Debug("Moto aded" + motorcycle.Name);
+        }
+        public IEnumerable<Motorcycle> GetMotorcycles ()
+        {
+            Log.Debug("Get Motorcycle");
+            return _motosicle;
+        }
+    }
+}
